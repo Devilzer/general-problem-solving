8e5c71255b26d77a04c8946b3efbdb73a9b74a0c
Devilzer <jenadeepak724@gmail.com>
2021-01-21 16:18:37 +0530

    initial

---
+++ question1.js
@@ -0,0 +1,15 @@
+// 1. Find the number of islands Given a boolean 2D matrix, find the number of islands. A group of connected 1s forms an island.
+
+const matrix = [
+    [1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 1]
+    ];
+    var row = matrix.length;
+    var col = matrix[0].length;
+
+    
+
+console.log(matrix[0].length)
