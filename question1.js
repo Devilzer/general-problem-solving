60f0d805df9d57265c4b6a3741b02ffc794bca3b
Devilzer <jenadeepak724@gmail.com>
2021-01-21 17:25:12 +0530

    add question 1 solution

--- question1.js
+++ question1.js
@@ -7,9 +7,35 @@ const matrix = [
     [0, 0, 0, 0, 0],
     [1, 0, 1, 0, 1]
     ];
-    var row = matrix.length;
-    var col = matrix[0].length;
-
     
 
-console.log(matrix[0].length)
+    var numberOfIsland = function(mat){
+        const markIsland = (mat,x,y,r,c)=>{
+            if(x<0||x>=r ||y<0 || y>=c ||mat[x][y]!==1){
+                return;
+            }
+            mat[x][y] = 2;
+            //recursive call
+            markIsland(mat,x-1,y,r,c); //top
+            markIsland(mat,x-1,y+1,r,c); //top-right
+            markIsland(mat,x,y+1,r,c); //right
+            markIsland(mat,x+1,y+1,r,c); //bottom-right
+            markIsland(mat,x+1,y,r,c); //bottom 
+            markIsland(mat,x+1,y-1,r,c); //bottom -left
+            markIsland(mat,x,y-1,r,c); //left
+            markIsland(mat,x-1,y-1,r,c); //top-left
+        }
+
+        var count = 0;
+        for(let i=0;i<mat.length;i++){
+            for(let j=0;j<mat[0].length;j++){
+                if(mat[i][j]===1){
+                    markIsland(mat,i,j,mat.length,mat[0].length);
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+
+console.log(numberOfIsland(matrix));
